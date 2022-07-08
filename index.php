a74a4128d7e5a767f6bdbe2f1b5a9c9f31a1c1c3
tongdung13 <tongvanthienvu@gmail.com>
2022-07-08 16:59:13 +0700

    update

--- index.php
+++ index.php
@@ -6,11 +6,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href="./datepicker/date-picker.css" rel="stylesheet">
-
+    <style>
+        .date {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+        }
+    </style>
 </head>
 
 <body>
-    <div class="datepicker-here" data-language="en"></div>
+    <div class="date">
+        <div class="datepicker-here" data-language="en"></div>
+    </div>
+
 
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
     <script src="./datepicker/datepicker.js"> </script>
