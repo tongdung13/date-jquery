8e1088723c1bb6c74a966ffd1aafeb173de14e4a
tongdung13 <tongvanthienvu@gmail.com>
2022-07-08 16:34:29 +0700

    update

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title></title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="./datepicker/date-picker.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <div class="datepicker-here" data-language="en"></div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="./datepicker/datepicker.js"> </script>
+    <script src="./datepicker/datepicker.en.js"></script>
+
+</body>
+
+</html>
